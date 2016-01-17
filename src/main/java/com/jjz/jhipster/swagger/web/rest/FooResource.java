ae033483ba823ef140193536c5b168818ff16f71
JJ Zabkar <jjzabkar@gmail.com>
2016-01-17 12:17:28 -0800

    test Swagger @Api* annotations

--- src/main/java/com/jjz/jhipster/swagger/web/rest/FooResource.java
+++ src/main/java/com/jjz/jhipster/swagger/web/rest/FooResource.java
@@ -1,10 +1,12 @@
 package com.jjz.jhipster.swagger.web.rest;
 
-import com.codahale.metrics.annotation.Timed;
-import com.jjz.jhipster.swagger.domain.Foo;
-import com.jjz.jhipster.swagger.repository.FooRepository;
-import com.jjz.jhipster.swagger.web.rest.util.HeaderUtil;
-import com.jjz.jhipster.swagger.web.rest.util.PaginationUtil;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -13,13 +15,20 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Optional;
+import com.codahale.metrics.annotation.Timed;
+import com.jjz.jhipster.swagger.domain.Foo;
+import com.jjz.jhipster.swagger.repository.FooRepository;
+import com.jjz.jhipster.swagger.web.rest.util.HeaderUtil;
+import com.jjz.jhipster.swagger.web.rest.util.PaginationUtil;
+
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
 
 /**
  * REST controller for managing Foo.
@@ -40,7 +49,7 @@ public class FooResource {
         method = RequestMethod.POST,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
-    public ResponseEntity<Foo> createFoo(@RequestBody Foo foo) throws URISyntaxException {
+	public ResponseEntity<Foo> createFoo(@ApiParam(value = "foo brief description jjz1") @RequestBody Foo foo) throws URISyntaxException {
         log.debug("REST request to save Foo : {}", foo);
         if (foo.getId() != null) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("foo", "idexists", "A new foo cannot already have an ID")).body(null);
@@ -113,4 +122,18 @@ public class FooResource {
         fooRepository.delete(id);
         return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("foo", id.toString())).build();
     }
+
+	/**
+	 * GET /foos -> get all the foos.
+	 * 
+	 * @throws URISyntaxException
+	 */
+	@RequestMapping(value = "/jjzfoos", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiResponse(message = "jjz444ApiResponse", code = 444)
+	@Timed
+	public ResponseEntity<List<Foo>> getAllFoosJJZ(@ApiParam(name = "jjzPageable", required = false) Pageable pageable)
+			throws URISyntaxException {
+		return this.getAllFoos(pageable);
+	}
+
 }
